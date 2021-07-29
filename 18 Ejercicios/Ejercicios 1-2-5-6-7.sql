84ac319c565cf93eb083148c81cb1b66cba94fd0
Amc210 <annamarin513@gmail.com>
2021-07-29 18:00:18 +0200

    Ejercicios 1-2-5-6-7

--- 18 Ejercicios/Ejercicios 1-2-5-6-7.sql
+++ 18 Ejercicios/Ejercicios 1-2-5-6-7.sql
@@ -397,6 +397,370 @@ LOCK TABLES `tiposPieza` WRITE;
 INSERT INTO `tiposPieza` VALUES ('tipo1','descripcion1'),('tipo2','descripcion2'),('tipo3','descripcion3');
 /*!40000 ALTER TABLE `tiposPieza` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Current Database: `UD12Ejercicio7`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `UD12Ejercicio7` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `UD12Ejercicio7`;
+
+--
+-- Table structure for table `categoriaProfesional`
+--
+
+DROP TABLE IF EXISTS `categoriaProfesional`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categoriaProfesional` (
+  `id` int NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoriaProfesional`
+--
+
+LOCK TABLES `categoriaProfesional` WRITE;
+/*!40000 ALTER TABLE `categoriaProfesional` DISABLE KEYS */;
+INSERT INTO `categoriaProfesional` VALUES (1,'nombre1'),(2,'nombre2'),(3,'nombre3');
+/*!40000 ALTER TABLE `categoriaProfesional` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contratos`
+--
+
+DROP TABLE IF EXISTS `contratos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contratos` (
+  `nContrato` int NOT NULL,
+  `fechaInicio` datetime DEFAULT NULL,
+  `fechaFinal` datetime DEFAULT NULL,
+  `idTrab` int DEFAULT NULL,
+  `categoria` int DEFAULT NULL,
+  PRIMARY KEY (`nContrato`),
+  KEY `idTrab` (`idTrab`),
+  KEY `categoria` (`categoria`),
+  CONSTRAINT `contratos_ibfk_1` FOREIGN KEY (`idTrab`) REFERENCES `trabajadores` (`id`),
+  CONSTRAINT `contratos_ibfk_2` FOREIGN KEY (`categoria`) REFERENCES `categoriaProfesional` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contratos`
+--
+
+LOCK TABLES `contratos` WRITE;
+/*!40000 ALTER TABLE `contratos` DISABLE KEYS */;
+INSERT INTO `contratos` VALUES (1,'2020-12-12 00:00:00','2021-12-12 00:00:00',1,3),(2,'2020-11-12 00:00:00','2021-11-12 00:00:00',2,2),(3,'2020-10-12 00:00:00','2021-10-12 00:00:00',3,1);
+/*!40000 ALTER TABLE `contratos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `departamentos`
+--
+
+DROP TABLE IF EXISTS `departamentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `departamentos` (
+  `nDept` int NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`nDept`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departamentos`
+--
+
+LOCK TABLES `departamentos` WRITE;
+/*!40000 ALTER TABLE `departamentos` DISABLE KEYS */;
+INSERT INTO `departamentos` VALUES (1,'nombre1'),(2,'nombre2'),(3,'nombre3');
+/*!40000 ALTER TABLE `departamentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nominas`
+--
+
+DROP TABLE IF EXISTS `nominas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nominas` (
+  `nContrato` int NOT NULL,
+  `salario` int DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `idTrab` int DEFAULT NULL,
+  PRIMARY KEY (`nContrato`),
+  KEY `idTrab` (`idTrab`),
+  CONSTRAINT `nominas_ibfk_1` FOREIGN KEY (`nContrato`) REFERENCES `contratos` (`nContrato`),
+  CONSTRAINT `nominas_ibfk_2` FOREIGN KEY (`idTrab`) REFERENCES `trabajadores` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nominas`
+--
+
+LOCK TABLES `nominas` WRITE;
+/*!40000 ALTER TABLE `nominas` DISABLE KEYS */;
+INSERT INTO `nominas` VALUES (1,69,'2020-10-12 00:00:00',3),(2,969,'2002-11-12 00:00:00',2),(3,669,'2020-12-12 00:00:00',1);
+/*!40000 ALTER TABLE `nominas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trabajadores`
+--
+
+DROP TABLE IF EXISTS `trabajadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trabajadores` (
+  `id` int NOT NULL,
+  `dni` varchar(100) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `telefono` int DEFAULT NULL,
+  `nSs` int DEFAULT NULL,
+  `ndept` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ndept` (`ndept`),
+  CONSTRAINT `trabajadores_ibfk_1` FOREIGN KEY (`ndept`) REFERENCES `departamentos` (`nDept`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trabajadores`
+--
+
+LOCK TABLES `trabajadores` WRITE;
+/*!40000 ALTER TABLE `trabajadores` DISABLE KEYS */;
+INSERT INTO `trabajadores` VALUES (1,NULL,'nombre1','apellido1','direcion1',332423,9259,1),(2,NULL,'nombre2','apellido2','direcion2',434534,9255,1),(3,NULL,'nombre3','apellido3','direcion3',234343,9245,1);
+/*!40000 ALTER TABLE `trabajadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Current Database: `UD12Ejercicio6`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `UD12Ejercicio6` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `UD12Ejercicio6`;
+
+--
+-- Table structure for table `autores`
+--
+
+DROP TABLE IF EXISTS `autores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `autores` (
+  `id` int NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `pais` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `autores`
+--
+
+LOCK TABLES `autores` WRITE;
+/*!40000 ALTER TABLE `autores` DISABLE KEYS */;
+INSERT INTO `autores` VALUES (1,'nombre1','pais1'),(2,'nombre2','pais2'),(3,'nombre3','pais13');
+/*!40000 ALTER TABLE `autores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cd`
+--
+
+DROP TABLE IF EXISTS `cd`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cd` (
+  `id` int NOT NULL,
+  `canciones` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `cd_ibfk_1` FOREIGN KEY (`id`) REFERENCES `obra` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cd`
+--
+
+LOCK TABLES `cd` WRITE;
+/*!40000 ALTER TABLE `cd` DISABLE KEYS */;
+INSERT INTO `cd` VALUES (1,65),(2,35),(3,95);
+/*!40000 ALTER TABLE `cd` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `copias`
+--
+
+DROP TABLE IF EXISTS `copias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `copias` (
+  `nCopias` int NOT NULL,
+  `deteriorada` tinyint(1) DEFAULT NULL,
+  `comentarios` varchar(100) DEFAULT NULL,
+  `idObra` int NOT NULL,
+  PRIMARY KEY (`nCopias`,`idObra`),
+  KEY `idObra` (`idObra`),
+  CONSTRAINT `copias_ibfk_1` FOREIGN KEY (`idObra`) REFERENCES `obra` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `copias`
+--
+
+LOCK TABLES `copias` WRITE;
+/*!40000 ALTER TABLE `copias` DISABLE KEYS */;
+INSERT INTO `copias` VALUES (3,1,'com2',2),(23,0,'com1',1),(32,0,'com3',3);
+/*!40000 ALTER TABLE `copias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libro`
+--
+
+DROP TABLE IF EXISTS `libro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libro` (
+  `id` int NOT NULL,
+  `npaginas` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `libro_ibfk_1` FOREIGN KEY (`id`) REFERENCES `obra` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libro`
+--
+
+LOCK TABLES `libro` WRITE;
+/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
+INSERT INTO `libro` VALUES (1,365),(2,552),(3,295);
+/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `obra`
+--
+
+DROP TABLE IF EXISTS `obra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `obra` (
+  `id` int NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `año` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `obra`
+--
+
+LOCK TABLES `obra` WRITE;
+/*!40000 ALTER TABLE `obra` DISABLE KEYS */;
+INSERT INTO `obra` VALUES (1,'nombre1',2019),(2,'nombre2',2000),(3,'nombre3',2020);
+/*!40000 ALTER TABLE `obra` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pelicula`
+--
+
+DROP TABLE IF EXISTS `pelicula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pelicula` (
+  `id` int NOT NULL,
+  `duracion` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `pelicula_ibfk_1` FOREIGN KEY (`id`) REFERENCES `obra` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pelicula`
+--
+
+LOCK TABLES `pelicula` WRITE;
+/*!40000 ALTER TABLE `pelicula` DISABLE KEYS */;
+INSERT INTO `pelicula` VALUES (1,96),(2,120),(3,85);
+/*!40000 ALTER TABLE `pelicula` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prestamos`
+--
+
+DROP TABLE IF EXISTS `prestamos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prestamos` (
+  `id` int NOT NULL,
+  `fPrestamo` datetime DEFAULT NULL,
+  `fDevolucion` datetime DEFAULT NULL,
+  `fTope` datetime DEFAULT NULL,
+  `nCopias` int NOT NULL,
+  PRIMARY KEY (`id`,`nCopias`),
+  KEY `nCopias` (`nCopias`),
+  CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`nCopias`) REFERENCES `copias` (`nCopias`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prestamos`
+--
+
+LOCK TABLES `prestamos` WRITE;
+/*!40000 ALTER TABLE `prestamos` DISABLE KEYS */;
+INSERT INTO `prestamos` VALUES (1,'2020-02-01 00:00:00','2020-03-01 00:00:00','2020-03-01 00:00:00',3),(2,'2020-04-01 00:00:00','2020-05-01 00:00:00','2020-05-01 00:00:00',23),(3,'2020-06-01 00:00:00','2020-06-01 00:00:00','2020-07-01 00:00:00',32);
+/*!40000 ALTER TABLE `prestamos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `socios`
+--
+
+DROP TABLE IF EXISTS `socios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `socios` (
+  `id` int DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `telefono` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `socios`
+--
+
+LOCK TABLES `socios` WRITE;
+/*!40000 ALTER TABLE `socios` DISABLE KEYS */;
+INSERT INTO `socios` VALUES (1,'nombre1','apellido1','direcion1',332423),(2,'nombre2','apellido2','direcion2',434534),(3,'nombre3','apellido3','direcion3',234343);
+/*!40000 ALTER TABLE `socios` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -407,4 +771,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-29 14:56:35
+-- Dump completed on 2021-07-29 17:59:25
